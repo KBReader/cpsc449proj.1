47b109edb863af26d80c51590583e7a730b06d2d
Kevin_BN <70188715+KBReader@users.noreply.github.com>
2024-10-04 11:47:19 -0700

    Update movie_control.py
    added 2 more APIs

--- movie_control.py
+++ movie_control.py
@@ -18,16 +18,52 @@ def add_movie():
 
         return jsonify({'message': 'Movie added successfully'}), 201
     
-@app.route('/delete/<title>', methods=['DELETE'])
+@app.route('/movies/<movie_id>', methods=['DELETE'])
 @admin_required
-def delete_movie(title):
-    movie = Movie.query.get(title)
+def delete_movie(movie_id):
+    movie = Movie.query.get(movie_id)
     if not movie:
-        return jsonify({'message': 'Movie not found'}), 404
+        return jsonify({'message': 'Movie ID not found'}), 404
 
     db.session.delete(movie)
     db.session.commit()
 
-    return jsonify({'message': 'Movie deleted successfully'}), 200
+    return jsonify({'message': 'Movie ID deleted successfully'}), 200
 
 
+@app.route('/admin/ratings/<rating_id>', methods=['DELETE'])
+@admin_required
+def delete_rating(rating_id):
+    rating = Rating.query.get(rating_id)
+    if not rating:
+        return jsonify({'message': 'Rating ID not found'}), 404
+    
+    db.session.delete(rating)
+    db.session.commit()
+    
+    return jsonify({'message': 'Rating ID deleted succesfully'}), 200
+
+@app.route('/ratings/<rating_id>', methods=['DELETE'])
+@login_required
+def delete_rating(rating_id, user_id):
+    rating = Rating.query.get(rating_id)
+    user = User.query.get(user_id)
+
+    if not rating:
+        return jsonify({'message': 'Rating ID not found'}), 404
+    
+    #make sure if the rating belongs to the current user
+    if rating.user_id != user_id:
+        return jsonify({'message': 'The rating ID is not posted by you'}), 403
+    
+    de.session.delete(rating)
+    db.session.commit()
+    return jsonify({'message': 'Rating ID deleted succesfully'}), 200
+
+# retrieves all ratings from all users
+@app.route('/ratings/', methods =['GET'])
+@login_required
+def fetchall_ratings():
+    ratings = db.session.scalars(db.select(Rating)).fetchall()
+    rating_list = [rating.to_dict() for rating in ratings]
+    return jsonify(rating_list)
