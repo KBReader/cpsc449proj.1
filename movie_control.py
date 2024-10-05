fdbd9913964dc1c8c4acdda2e6c8c3ef3e534d97
Kevin_BN <70188715+KBReader@users.noreply.github.com>
2024-10-04 17:21:49 -0700

    Update movie_control.py

--- movie_control.py
+++ movie_control.py
@@ -61,9 +61,32 @@ def delete_rating(rating_id, user_id):
     return jsonify({'message': 'Rating ID deleted succesfully'}), 200
 
 # retrieves all ratings from all users
-@app.route('/ratings/', methods =['GET'])
+@app.route('/ratings', methods =['GET'])
 @login_required
 def fetchall_ratings():
-    ratings = db.session.scalars(db.select(Rating)).fetchall()
+    ratings = db.session.scalars(db.select(Rating)).all() #does .fetchall or .all works?
     rating_list = [rating.to_dict() for rating in ratings]
     return jsonify(rating_list)
+
+@app.route('/ratings/<rating_id>', methods =['PUT'])
+@login_required
+def update_rating(rating_id):
+
+    rating = Rating.query.get(rating_id)
+    user_id = current_user.id
+
+    if not rating:
+        return jsonify ({'message': 'Rating ID not found'}), 404
+    
+    if rating.user_id != user_id:
+        return jsonify({'message': 'The rating ID is not posted by you'}), 403
+
+    data = request.get_json()
+    new_draft = jsonify('draft')
+
+    if new_draft is None:
+        return jsonify({'message': 'There is nothing in your rating'}), 400
+    
+    rating.draft = new_draft #key to updating
+    db.session.commit()
+    return jsonify({'message': 'Rating ID has been updated'}), 200
