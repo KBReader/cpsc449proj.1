d8667f0f26db8f78a8bcbcd9c1b7eec1a22fcb86
Kevin_BN <70188715+KBReader@users.noreply.github.com>
2024-10-05 21:15:03 -0700

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,21 @@
+from flask import Flask,request,jsonify
+import os
+
+@app.route('./upload', methods=['POST'])
+def upload_file():
+
+    if 'file' not in request.files:
+        return jsonify({'message': 'File does not exist'}), 400
+    
+    file = request.files['file']
+
+    if file.filename = '':
+        return jsonify({'message': 'File is not selected'}), 400
+
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return jsonify({'message': 'File '{file.filename}' uploaded successfully!'}), 200
+
+if __name__= '__main__':
+    app.run(debug = True)
