ebd388aadffc697659e15433120894101a7f8fac
Johnny Quach <84772584+JqJohnny@users.noreply.github.com>
2024-10-11 15:54:34 -0700

    Small error

--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,jsonify
 import os
 
+
 @app.route('./upload', methods=['POST'])
 def upload_file():
 
@@ -17,5 +18,5 @@ def upload_file():
         file.save(filepath)
         return jsonify({'message': 'File '{file.filename}' uploaded successfully!'}), 200
 
-if __name__= '__main__':
+if __name__ == '__main__':
     app.run(debug = True)
